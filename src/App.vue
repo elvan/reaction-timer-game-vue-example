c7ae76db66a92888eac8f3e89a5c97e205c4651a
Elvan H <elvanhd@gmail.com>
2022-12-24 17:32:26 +0700

    starting a new game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <h1>Reaction Timer Game</h1>
+
+  <button :disabled="isPlaying" @click="start">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/Block.vue';
 export default {
   name: 'App',
 
-  components: {},
+  components: {
+    Block,
+  },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
 };
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">Click Me</div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+
+  props: {
+    delay: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
