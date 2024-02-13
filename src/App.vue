3c84f908f32207cd9391aca81791d81fcde119bf
Elvan Hidayat <elvanhd@gmail.com>
2024-02-13 19:13:27 +0700

    Add project setup and initial files
    - Add .browserslistrc for browser compatibility settings
    - Include .editorconfig for consistent coding styles
    - Update .gitignore to exclude node_modules, logs, and env files
    - Configure Babel with Vue CLI plugin in babel.config.js
    - Set up jsconfig.json for better development experience
    - Set up package.json with Vue 3 and build scripts
    - Add favicon and basic index.html setup for the web app
    - Create initial Vue app structure in src/App.vue and main.js

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Reaction Timer</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  margin-top: 60px;
+  color: #444;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
